bf1aeb71b25262db42d15e992b0011f054bb34ff
Pavlyukovsky <Pro10097Sasha@gmail.com>
2016-08-13 00:14:28 +0300

    Update runPopup.php

--- runPopup.php
+++ runPopup.php
@@ -4,13 +4,9 @@ include_once 'Db.php';
 
 if(isset($_POST['fileName'])){
     $fileName = $_POST['fileName'];
-    // $fileName = "2.txt";
     $dir = 'downloads/';
     $content = file_get_contents($dir.$fileName);
 
     echo $content;
 }
-
-
-
 ?>
